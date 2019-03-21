fc4f8eae39e59eeb65324a6c5616474bec1aa93a
elishalmon <elishalmon111@gmail.com>
2019-03-21 15:09:58 +0200

    initial userDetails sql script

--- SQL SPRINT1/userDetails.sql
+++ SQL SPRINT1/userDetails.sql
@@ -1,5 +1,7 @@
-DROP table degree;
-CREATE table degree(
-    degreeId  VARCHAR(50) PRIMARY KEY,
-    degreeName VARCHAR(50) NOT NULL
+DROP table userDetails;
+CREATE table userDetails(
+    userId  VARCHAR(50) PRIMARY KEY,
+    collegeName VARCHAR(50) NOT NULL,
+    yearOfStudy VARCHAR(50) NOT NULL,
+    aboutMe VARCHAR(100) NOT NULL
 );
