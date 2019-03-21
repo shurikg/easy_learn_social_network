a2feaa6ef9929bfe7e4a5d0f9b2b0993a5364340
elishalmon <elishalmon111@gmail.com>
2019-03-21 16:09:58 +0200

    delete SQL SPRINT1 directory

--- SQL SPRINT1/userCourses.sql
+++
@@ -1,7 +0,0 @@
-DROP table userCourses;
-CREATE table userCourses(
-    CONSTRAINT fk_userId FOREIGN KEY (userId) REFERENCES userDetails(userId),
-    CONSTRAINT fk_courseId FOREIGN KEY (courseId) REFERENCES courses(courseId),
-    PRIMARY KEY (userId, courseId )
-);
-
