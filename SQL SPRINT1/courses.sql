1c84ded6359679f6767d23346aebe44bba98d80b
elishalmon <elishalmon111@gmail.com>
2019-03-21 16:06:59 +0200

    delete SQL SPRINT1 directory

--- SQL SPRINT1/courses.sql
+++
@@ -1,5 +0,0 @@
-DROP table courses;
-CREATE table courses(
-    courseId  VARCHAR(50) PRIMARY KEY,
-    courseName  VARCHAR(50) NOT NULL
-);
--- SQL SPRINT1/degree.sql
+++
@@ -1,5 +0,0 @@
-DROP table degree;
-CREATE table degree(
-    degreeId  VARCHAR(50) PRIMARY KEY,
-    degreeName  VARCHAR(50)  NOT NULL
-);
