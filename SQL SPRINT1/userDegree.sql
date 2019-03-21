3bacbbc6ae5600fd967e0292ad35e1dbd6e13ab7
elishalmon <elishalmon111@gmail.com>
2019-03-21 15:08:55 +0200

    initial userDegree sql script

--- SQL SPRINT1/userDegree.sql
+++ SQL SPRINT1/userDegree.sql
@@ -1,10 +1,6 @@
 DROP table userDegree;
-
 CREATE table userDegree(
-   
- CONSTRAINT fk_userId FOREIGN KEY (userId) REFERENCES userDetails(userId),
-   
- CONSTRAINT fk_degreeId FOREIGN KEY (degreeId) REFERENCES degree(degreeId),
-    
-PRIMARY KEY(userId, degreeId)
+    CONSTRAINT fk_userId FOREIGN KEY (userId) REFERENCES userDetails(userId),
+    CONSTRAINT fk_degreeId FOREIGN KEY (degreeId) REFERENCES degree(degreeId),
+    PRIMARY KEY (userId, degreeId)
 );
