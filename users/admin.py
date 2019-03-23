b7a50a0a806805823d7333c1ee44ecedd012e3a3
Yoni Cohen <jonbir3@gmail.com>
2019-03-23 13:13:51 +0200

    register the following models to control by the admin: - Degree - Course - Profile - UserDegree - UserCourse

---
+++ users/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Degree)
+admin.site.register(Course)
+admin.site.register(Profile)
+admin.site.register(UserDegrees)
+admin.site.register(UserCourses)
