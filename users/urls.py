c3e1c403adc5d91b61b52a1562c594181cc12b58
Tofahi <shlomitofahi@gmail.com>
2019-03-23 23:45:36 +0200

    add urls of edit profile

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.RegisterFormWizard.as_view(), name='register-wizard'),
+    path('home/', views.home, name='home'),
+    path('profile/', views.view_profile, name='view_profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('profile/edit/personal-info/', views.edit_personal_info, name='edit_personal_info'),
+    path('profile/edit/more-info/', views.edit_more_info, name='edit_more_info'),
+    path('profile/edit/change-password/', views.change_password, name='change_password'),
+    url(r'^register/$', views.register, name='register'),
 ]
