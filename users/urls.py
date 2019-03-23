e5218e83ef9624c30de2b6506518a492f8590103
May hagbi <mayhagbi@gmail.com>
2019-03-24 00:55:06 +0200

    add login path

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.RegisterFormWizard.as_view(), name='register-wizard'),
+    path('register/', views.RegisterFormWizard.as_view(), name='register-wizard'),
+    # url('login/', views.login_page, name='login'),
+    url('profile/', views.profile, name='home'),
+    url('home/', views.home_page, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login')
+    # url('', views.home_page, name='home'),
 ]
