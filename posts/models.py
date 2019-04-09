5bda6ec1f48465b840fad79f4ccce3ec53a6cae7
May hagbi <mayhagbi@gmail.com>
2019-04-09 10:47:48 +0300

    T19-125 adding option for adding new post and see all posts that created.

--- posts/models.py
+++ posts/models.py
@@ -3,15 +3,16 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     category = models.CharField(max_length=50)
     body = models.TextField(max_length=5000)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.category
+
 
 class Comments(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField(max_length=5000)
     postId = models.ForeignKey(Post, on_delete=models.CASCADE)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewPost
+from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+
+def home_posts(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'home_posts.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date']
+
+
+def create_new_post(request):
+    if request.method == 'POST':
+        form = NewPost(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('posts:home')
+    form = NewPost
+    return render(request, 'new_post.html', {"form": form})
+
+
+
+
+# class PostDetailView(DetailView):
+#     model = Post
