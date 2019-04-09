d045361b8a11eeec185eadf1c358700a14c49268
Tofahi <shlomitofahi@gmail.com>
2019-04-09 10:07:38 +0300

    add the new pages to urls

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('profile/edit/personal-info/', views.edit_personal_info, name='edit_personal_info'),
     path('profile/edit/more-info/', views.edit_more_info, name='edit_more_info'),
+    path('profile/edit/privacy', views.edit_privacy, name='edit_privacy'),
     path('profile/edit/change-password/', views.change_password, name='change_password'),
+    path('show-users/', views.show_users, name='show_users'),
+    path('show-users/selected-user/', views.show_selected_user, name='show_users_all'),
+    path('show-users/results/', views.search_result, name='search'),
 ]
