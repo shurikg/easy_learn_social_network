0208d3f14d42c09314565372222602a21e3e0474
May hagbi <mayhagbi@gmail.com>
2019-04-13 15:39:30 +0300

    T19-136 adding to - creating new post,  categories choice of 'other' and all exists courses

--- posts/forms.py
+++ posts/forms.py
@@ -6,21 +6,16 @@ from users.models import Course
 class NewPost(forms.ModelForm):
     CATEGORY_CHOICES = (
         ('other', 'Other'),
-        ('study', 'Study'),
     )
+    COURSES = tuple(map(lambda course_name: (course_name, course_name), Course.objects.only('course_name')))
+    category_list = CATEGORY_CHOICES + COURSES
 
-    item_field = forms.ModelChoiceField(queryset=Course.objects.only('course_name'))
-
-    # def __init__(self):
-    #     super(NewPost, self).__init__()
-    #     self.fields['item_field'].queryset = Course.objects.filter(course_name=)
-
-    category = forms.ChoiceField(help_text='Select category', choices=CATEGORY_CHOICES)
+    category = forms.ChoiceField(help_text='Select category', choices=category_list)
     body = forms.CharField(max_length=5000, widget=forms.Textarea)
 
     class Meta:
         model = Post
-        fields = ('category', 'item_field', 'body',)
+        fields = ('category', 'body',)
 
 
 class Comment(forms.ModelForm):
