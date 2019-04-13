0fca2ec859f8d13364932e7e512164ef688a6231
May hagbi <mayhagbi@gmail.com>
2019-04-13 13:48:37 +0300

    change url in posts

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from posts.models import Post, Comments
+from users.models import Course
 
 
 class NewPost(forms.ModelForm):
@@ -8,12 +9,18 @@ class NewPost(forms.ModelForm):
         ('study', 'Study'),
     )
 
+    item_field = forms.ModelChoiceField(queryset=Course.objects.only('course_name'))
+
+    # def __init__(self):
+    #     super(NewPost, self).__init__()
+    #     self.fields['item_field'].queryset = Course.objects.filter(course_name=)
+
     category = forms.ChoiceField(help_text='Select category', choices=CATEGORY_CHOICES)
     body = forms.CharField(max_length=5000, widget=forms.Textarea)
 
     class Meta:
         model = Post
-        fields = ('category', 'body',)
+        fields = ('category', 'item_field', 'body',)
 
 
 class Comment(forms.ModelForm):
