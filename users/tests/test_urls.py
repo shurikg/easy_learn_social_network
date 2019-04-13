bfadc58f7a67f7e7938e68649cac1456d09edd6e
jonbir3 <jonbir3@gmail.com>
2019-04-14 01:06:32 +0300

    T19-178 unit test to list of friends url

--- users/tests/test_urls.py
+++ users/tests/test_urls.py
@@ -1,9 +1,20 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse, resolve
 from users.views import *
 
 
-class TestUrls(SimpleTestCase):
+class TestUrls(TestCase):
+
+    def setUp(self):
+        # create user
+        username = 'testuser'
+        email = 'test@test.com'
+        password = '12345'
+        self.user = User(username=username, email=email)
+        self.user.set_password(password)
+        self.user.save()
+        login = self.client.login(username=username, password=password)
+        self.assertEqual(login, True)
 
     def test_test_url_is_resolved(self):
         assert 1 == 1
@@ -15,3 +26,10 @@ class TestUrls(SimpleTestCase):
     def test_edit_profile_url_is_resolved(self):
         url = reverse('users:edit_profile')
         self.assertEqual(resolve(url).func, edit_profile)
+
+    def test_list_of_friends_url_is_is_resolved(self):
+        user_id = self.user.id
+        url = reverse('users:list_of_friends', args=(user_id,))
+        self.assertEqual(resolve(url).func, list_of_friends)
+
+
