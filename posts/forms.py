b5b8777502ff376f43442042083fd9f7f5f6771f
May hagbi <mayhagbi@gmail.com>
2019-04-14 15:31:19 +0300

    T19-124 create option to category with "other".

--- posts/forms.py
+++ posts/forms.py
@@ -11,9 +11,10 @@ class NewPost(forms.ModelForm):
     )
     COURSES = tuple(map(lambda course_name: (course_name, course_name), Course.objects.only('course_name')))
     category_list = CATEGORY_CHOICES + COURSES
+    category_rules = 'By selecting the "Other" option, only your friends can view the post. ' \
+                     'Otherwise the post will be set to public.'
 
-    category = forms.ChoiceField(help_text='By selecting the "Other" option, only your friends can view the post. ' +
-                                           'Otherwise the post will be set to public.', choices=category_list)
+    category = forms.ChoiceField(help_text=category_rules, choices=category_list)
     body = forms.CharField(max_length=5000, widget=forms.Textarea)
 
     class Meta:
