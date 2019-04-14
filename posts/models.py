974891b6da526ea4126422ee617024142c7a7379
May hagbi <mayhagbi@gmail.com>
2019-04-14 15:20:31 +0300

    T19-132 Create model Post.

--- posts/models.py
+++ posts/models.py
@@ -8,8 +8,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.category
+    # def __str__(self):
+    #     return self.category
 
 
 class Comments(models.Model):
