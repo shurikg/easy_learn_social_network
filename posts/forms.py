1ebc8250e1cbf95ba9ca99a7fe6cf1231083a3a5
jonbir3 <jonbir3@gmail.com>
2019-04-24 18:17:48 +0300

    add exception handling to the COURSES attribute of NewPost form

--- posts/forms.py
+++ posts/forms.py
@@ -9,7 +9,11 @@ class NewPost(forms.ModelForm):
     CATEGORY_CHOICES = (
         (OTHER_CATEGORY, 'Other'),
     )
-    COURSES = tuple(map(lambda course_name: (course_name, course_name), Course.objects.only('course_name')))
+    COURSES = ()
+    try:
+        COURSES = tuple(map(lambda course_name: (course_name, course_name), Course.objects.only('course_name')))
+    except Exception as e:
+        print(e)
     category_list = CATEGORY_CHOICES + COURSES
     category_rules = 'By selecting the "Other" option, only your friends can view the post. ' \
                      'Otherwise the post will be set to public.'
