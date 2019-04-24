d97cb97ce4f3a590817a2109570811d7f6a568be
jonbir3 <jonbir3@gmail.com>
2019-04-24 17:13:26 +0300

    add mock of Course model

--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from posts import views
+from users.models import Course
 
 
 class TestUrls(TestCase):
+
+    def setUp(self):
+        self.oop_course = Course(1, 'OOP')
+        self.oop_course.save()
 
     def test_view_create_new_post_url_is_resolved(self):
         url = reverse('posts:newPost')
