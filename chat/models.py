d5e364a0570c9035b1b76ec6b713804fdfcddaea
Yoni <jonbir3@gmail.com>
2019-04-26 21:01:53 +0300

    T19-280 create model Message

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,92 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+
+class MessageManager(models.Manager):
+
+    def inbox_for(self, user):
+        """
+        Returns all messages that were received by the given user and are not
+        marked as deleted.
+        """
+        return self.filter(
+            recipient=user,
+            recipient_deleted_at__isnull=True,
+        )
+
+    def outbox_for(self, user):
+        """
+        Returns all messages that were sent by the given user and are not
+        marked as deleted.
+        """
+        return self.filter(
+            sender=user,
+            sender_deleted_at__isnull=True,
+        )
+
+    def trash_for(self, user):
+        """
+        Returns all messages that were either received or sent by the given
+        user and are marked as deleted.
+        """
+        return self.filter(
+            recipient=user,
+            recipient_deleted_at__isnull=False,
+        ) | self.filter(
+            sender=user,
+            sender_deleted_at__isnull=False,
+        )
+
+
+class Message(models.Model):
+    """
+    A private message from user to user
+    """
+    subject = models.CharField(max_length=50)
+    body = models.TextField(max_length=1500)
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    recipient = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    parent_msg = models.ForeignKey('self', related_name='next_messages', null=True, blank=True, on_delete=models.CASCADE)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    read_at = models.DateTimeField(null=True, blank=True)
+    replied_at = models.DateTimeField(null=True, blank=True)
+    sender_deleted_at = models.DateTimeField(null=True, blank=True)
+    recipient_deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = MessageManager()
+
+    def new(self):
+        """returns whether the recipient has read the message or not"""
+        if self.read_at is not None:
+            return False
+        return True
+
+    def replied(self):
+        """returns whether the recipient has written a reply to this message"""
+        if self.replied_at is not None:
+            return True
+        return False
+
+    def __str__(self):
+        return self.subject
+
+    def get_absolute_url(self):
+        return reverse('messages_detail', args=(self.id,))
+
+    def save(self, **kwargs):
+        if not self.id:
+            self.sent_at = timezone.now()
+        super(Message, self).save(**kwargs)
+
+    class Meta:
+        ordering = ['-sent_at']
+
+
+def inbox_count_for(user):
+    """
+    returns the number of unread messages for the given user but does not
+    mark them seen
+    """
+    return Message.objects.filter(recipient=user, read_at__isnull=True, recipient_deleted_at__isnull=True).count()
