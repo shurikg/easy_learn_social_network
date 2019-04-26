d8e931c6d4e93cd3a6b05e1fac01f9fbf5bca20b
Yoni <jonbir3@gmail.com>
2019-04-26 21:42:01 +0300

    T19-281 add test for save message

---
+++ chat/tests/test_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from chat.models import Message
+from django.contrib.auth.models import User
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+
+        # create sender
+        recipient_username = 'sender'
+        recipient_email = 'sender@test.com'
+        recipient_password = '12345'
+        self.sender = User(username=recipient_username, email=recipient_email)
+        self.sender.set_password(recipient_password)
+        self.sender.save()
+        login = self.client.login(username=recipient_username, password=recipient_password)
+        self.assertEqual(login, True)
+
+        # create recipient
+        recipient_username = 'recipient'
+        recipient_email = 'recipient@test.com'
+        recipient_password = '12345'
+        self.recipient = User(username=recipient_username, email=recipient_email)
+        self.recipient.set_password(recipient_password)
+        self.recipient.save()
+        login = self.client.login(username=recipient_username, password=recipient_password)
+        self.assertEqual(login, True)
+
+        # create message
+        self.message = Message.objects.create(
+            subject='test subject',
+            body='test body',
+            sender=self.sender,
+            recipient=self.recipient,
+        )
+
+    def test_save_message(self):
+        message = Message.objects.get(subject='test subject')
+        self.assertIsNotNone(message)
