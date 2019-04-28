0f71a0840bdf4ee6638f8db981df718036ebc95c
Yoni <jonbir3@gmail.com>
2019-04-28 19:38:58 +0300

    T19-303 add create at field to the model

--- files/models.py
+++ files/models.py
@@ -7,6 +7,7 @@ class File(models.Model):
     file_type = models.CharField(max_length=10, default='')
     file_url = models.FileField(upload_to='files/', default='')
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_at = models.DateTimeField()
 
     def __str__(self):
         return self.file_name
