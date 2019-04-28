20177bbda450cb99f8b3a0d448b895ecf013aac8
Yoni <jonbir3@gmail.com>
2019-04-28 20:42:28 +0300

    T19-295 fix bug on make migrations

--- files/models.py
+++ files/models.py
@@ -4,15 +4,15 @@ from users.models import Course, Degree
 
 
 class File(models.Model):
-    file_name = models.CharField(max_length=30, default='')
-    file_type = models.CharField(max_length=10, default='')
+    file_name = models.CharField(max_length=30, null=True, blank=True)
+    file_type = models.CharField(max_length=10, null=True, blank=True)
     file_url = models.FileField(upload_to='files/', default='')
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    create_at = models.DateTimeField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    create_at = models.DateTimeField(null=True)
     upload_at = models.DateTimeField(auto_now_add=True)
-    file_size = models.DecimalField(max_digits=27, decimal_places=2)
-    category = models.ForeignKey(Course, on_delete=models.CASCADE)
-    related_degrees = models.ManyToManyField(Degree)
+    file_size = models.DecimalField(max_digits=27, decimal_places=2, null=True, blank=True)
+    category = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    related_degrees = models.ManyToManyField(Degree, blank=True)
 
     def __str__(self):
         return self.file_name
