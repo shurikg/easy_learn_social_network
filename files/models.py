759b26c95cef43e0632d57abdb11288e39db196d
Yoni <jonbir3@gmail.com>
2019-04-28 19:35:47 +0300

    T19-302 add owner field to the model

--- files/models.py
+++ files/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class File(models.Model):
     file_name = models.CharField(max_length=30, default='')
     file_type = models.CharField(max_length=10, default='')
     file_url = models.FileField(upload_to='files/', default='')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.file_name
