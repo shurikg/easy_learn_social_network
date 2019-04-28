e12bcd5cc660c16e3e0b7677be73e26ae63c8c4c
shlomi <shlomitofahi@gmail.om>
2019-04-28 20:34:04 +0300

    add show file func to views

--- files/views.py
+++ files/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render
 
 # Create your views here.
+from users.views import User
+
+
+@login_required
+def show_files(request):
+    query = request.GET.get('q')
+    if query:
+        users = User.objects.filter(Q(first_name=query) | Q(last_name=query))
+    else:
+        users = User.objects.all().order_by('last_name')
+    args = {'users': users}
+    return render(request, 'files/show_files.html', args)
