8ba1e747457345c1d0c669788e903abf9f292400
May hagbi <mayhagbi@gmail.com>
2019-04-28 23:19:25 +0300

    T19-240 create new view for adding file

--- files/urls.py
+++ files/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('show-files/', views.show_files, name='show_files'),
+    path('add-file/', views.add_new_file, name='add_file'),
 ]
--- files/views.py
+++ files/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render
+from files.forms import CreateNewFile
 
 # Create your views here.
 from users.views import User
@@ -15,3 +16,20 @@ def show_files(request):
         users = User.objects.all().order_by('last_name')
     args = {'users': users}
     return render(request, 'files/show_files.html', args)
+
+
+@login_required
+def add_new_file(request):
+    '''
+        if request.method == 'POST':
+        form = NewPost(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('posts:feed')
+    :param request:
+    :return:
+    '''
+    form = CreateNewFile
+    return render(request, 'files/new_file.html', {"form": form})
