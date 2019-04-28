a511b340ff3398ca53a065e9a430d3eca2ea5b96
Yoni <jonbir3@gmail.com>
2019-04-28 18:58:01 +0300

    T19-288 create a new File model and register it at the admin

--- files/models.py
+++ files/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    file_name = models.CharField(max_length=30, default='')
+    file_type = models.CharField(max_length=10, default='')
+    file_url = models.FileField(upload_to='files/', default='')
+
+    def __str__(self):
+        return self.file_name
