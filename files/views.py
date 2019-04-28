915a945950e941cb0679c5f4fc239b5f2d5d2fc8
shlomi <shlomitofahi@gmail.om>
2019-04-28 22:17:15 +0300

    more att for views

--- files/views.py
+++ files/views.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
 from django.shortcuts import render
 
-# Create your views here.
+from files.forms import SelectCategoryForm
+from files.models import File
 from users.views import User
 
 
 @login_required
 def show_files(request):
-    query = request.GET.get('q')
-    if query:
-        users = User.objects.filter(Q(first_name=query) | Q(last_name=query))
+    form = SelectCategoryForm(request.POST)
+    if request.method == 'POST':
+        category = form.cleaned_data.get('category')
+        files = File.objects.filter(category=category)
     else:
-        users = User.objects.all().order_by('last_name')
-    args = {'users': users}
+        files = File.objects.all()
+
+    args = {'files': files, 'form': form}
     return render(request, 'files/show_files.html', args)
