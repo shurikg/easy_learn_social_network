84f7a7df2e443b7b725623329850e896b9b71881
Yoni <jonbir3@gmail.com>
2019-04-28 19:40:11 +0300

    T19-304 add upload at field to the model

--- files/models.py
+++ files/models.py
@@ -8,6 +8,7 @@ class File(models.Model):
     file_url = models.FileField(upload_to='files/', default='')
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     create_at = models.DateTimeField()
+    upload_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.file_name
