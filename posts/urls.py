eb8b107e09cb270ebba989b1e7e5e06a1ea6ba65
shlomi <shlomitofahi@gmail.com>
2019-04-29 15:47:33 +0300

    add url of show file

--- posts/urls.py
+++ posts/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 from . import views
@@ -5,6 +6,8 @@ from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('show_post/<int:pk>/', login_required(PostDetailView.as_view()), name='post-detail'),
+    path('download-file/', views.download_file, name='download_file'),
+    url(r'^download-file/(?P<file_id>\d+)/$', views.download_file, name='download_file'),
     path('feed/', login_required(PostListView.as_view()), name='feed'),
     path('new/', views.create_new_post, name='newPost'),
 ]
