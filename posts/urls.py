c5899c8d3899b1f63992ab0ddfe5e5d9c304ea18
shlomi <shlomitofahi@gmail.com>
2019-04-29 15:56:09 +0300

    restore mistake

--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 from . import views
@@ -6,8 +5,6 @@ from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('show_post/<int:pk>/', login_required(PostDetailView.as_view()), name='post-detail'),
-    path('download-file/', views.download_file, name='download_file'),
-    url(r'^download-file/(?P<file_id>\d+)/$', views.download_file, name='download_file'),
     path('feed/', login_required(PostListView.as_view()), name='feed'),
     path('new/', views.create_new_post, name='newPost'),
 ]
