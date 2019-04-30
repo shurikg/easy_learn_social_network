76c0e2558fc3d4f91f3cae64052217bac0919cae
Yoni <jonbir3@gmail.com>
2019-04-30 23:49:59 +0300

    T19-303 change the field type from DateTime to Date

--- files/models.py
+++ files/models.py
@@ -11,7 +11,7 @@ class File(models.Model):
     file_type = models.CharField(max_length=10, null=True, blank=True)
     file_url = models.FileField(upload_to='files/', default='')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    create_at = models.DateTimeField(null=True)
+    create_at = models.DateField(null=True)
     upload_at = models.DateTimeField(auto_now_add=True)
     file_size = models.DecimalField(max_digits=27, decimal_places=2, null=True, blank=True)
     category = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
