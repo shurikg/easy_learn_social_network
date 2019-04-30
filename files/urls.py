56aa57f5d15a8377b96974dd8316ec0e1c1e836f
shlomi <shlomitofahi@gmail.com>
2019-04-30 10:05:47 +0300

    T19-213

--- files/urls.py
+++ files/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('show-files/', views.show_files, name='show_files'),
+   # path('show-files/', views.show_files, name='show_files'),
     path('add-file/', views.add_new_file, name='add_file'),
     path('download-file/', views.download_file, name='download_file'),
     url(r'^download-file/(?P<file_id>\d+)/$', views.download_file, name='download_file'),
