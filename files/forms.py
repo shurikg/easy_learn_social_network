26bb66d8c93d86c1dfb58427ba921d39d5d75e73
May hagbi <mayhagbi@gmail.com>
2019-05-01 19:17:53 +0300

    T19-267 create new form for file

--- files/forms.py
+++ files/forms.py
@@ -8,19 +8,11 @@ class DateInput(forms.DateInput):
 
 
 class CreateNewFile(forms.ModelForm):
-    COURSES = ()
-    DEGREES = ()
-    try:
-        COURSES = tuple(map(lambda course_name: (course_name, course_name), Course.objects.only('course_name')))
-        DEGREES = tuple(map(lambda degree: (degree, degree), Degree.objects.only('degree_name')))
-    except Exception as e:
-        print(e)
-
-    category = forms.ChoiceField(choices=COURSES)
-    related_degrees = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=DEGREES)
+    category = forms.ModelChoiceField(queryset=Course.objects.all())
+    related_degrees = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple, queryset=Degree.objects.all())
     create_at = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
-    file_url = forms.FileField(label='File')
+    # file_url = forms.FileField(label='File')
 
     class Meta:
-        model = Course
+        model = File
         fields = ('category', 'related_degrees', 'create_at', 'file_url',)
