fa842508806a5b5f0b86d9720a12e38a5c4dfac4
Yoni <jonbir3@gmail.com>
2019-05-02 22:25:47 +0300

    T19-299 add the owner username to the file name

--- files/models.py
+++ files/models.py
@@ -33,7 +33,7 @@ class File(models.Model):
         super(File, self).save(**kwargs)
         initial_path = self.file_url.path
         self.file_type = self.get_file_extension()
-        self.file_name = '' + str(self.id) + '.' + str(self.file_type)
+        self.file_name = '' + str(self.id) + '_' + str(self.owner) + '.' + str(self.file_type)
         new_path = settings.MEDIA_ROOT + '/' + UPLOAD_TO_DIR + self.file_name
         os.rename(initial_path, new_path)
         super(File, self).save(**kwargs)
