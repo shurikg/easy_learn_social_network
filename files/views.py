4e43ca1e4cbff9b20f15d83b51a23d326aecab3e
shlomi <shlomitofahi@gmail.com>
2019-05-03 00:52:06 +0300

    T19-222 get all file by filter from db

--- files/views.py
+++ files/views.py
@@ -12,6 +12,31 @@ from users.views import User
 def show_files(request):
     files = File.objects.all()
     form = categoryForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            category_filter = form.cleaned_data.get('category')
+            degree_filter = form.cleaned_data.get('degree')
+            user_filter = form.cleaned_data.get('user')
+            if category_filter:
+                if degree_filter:
+                    if user_filter:
+                        files = File.objects.filter(category=category_filter, related_degrees=degree_filter, owner=user_filter)
+                    else:
+                        files = File.objects.filter(category=category_filter, related_degrees=degree_filter)
+                elif user_filter:
+                    files = File.objects.filter(category=category_filter, owner=user_filter)
+                else:
+                    files = File.objects.filter(category=category_filter)
+            elif degree_filter:
+                if user_filter:
+                    files = File.objects.filter(related_degrees=degree_filter, owner=user_filter)
+                else:
+                    files = File.objects.filter(related_degrees=degree_filter)
+            elif user_filter:
+                files = File.objects.filter(owner=user_filter)
+            else:
+                files = File.objects.all()
+
     args = {'files': files, 'form': form}
     return render(request, 'files/show_files.html', args)
 
@@ -34,6 +59,5 @@ def add_new_file(request):
 def download_file(request, file_id):
     file = File.objects.get(id=file_id)
     args = {'file': file, }
-    print("id {}".format(file.file_url))
     return render(request, 'files/download_file.html', args)
 
