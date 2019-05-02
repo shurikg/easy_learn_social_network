3eef2b0377f8989f648a0fe86463bae0175db604
shlomi <shlomitofahi@gmail.com>
2019-05-02 22:02:47 +0300

    T19-214 User friendly interface css boostrap font and etc

--- files/views.py
+++ files/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render, redirect
-from files.forms import CreateNewFile
+from files.forms import CreateNewFile, categoryForm
 
 # Create your views here.
 from files.models import File
@@ -10,12 +10,9 @@ from users.views import User
 
 @login_required
 def show_files(request):
-    query = request.GET.get('q')
-    if query:
-        users = User.objects.filter(Q(first_name=query) | Q(last_name=query))
-    else:
-        users = User.objects.all().order_by('last_name')
-    args = {'users': users}
+    files = File.objects.all()
+    form = categoryForm(request.POST)
+    args = {'files1': files, 'form': form}
     return render(request, 'files/show_files.html', args)
 
 
