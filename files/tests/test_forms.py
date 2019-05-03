264f4944bfb0612e8af97dbcb046a5943d8e7ef9
shlomi <shlomitofahi@gmail.com>
2019-05-03 19:29:10 +0300

    T19-231 uni-test form Content filtering

--- files/tests/test_forms.py
+++ files/tests/test_forms.py
@@ -1,9 +1,16 @@
-from django.test import TestCase
+
+from django.test import TestCase, Client
 from files.forms import filterFilesForm
 
 
 class MyTests(TestCase):
+
     def test_filterFilesForm_forms(self):
         form_data = {'something': 'something'}
         form = filterFilesForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+    def test_form(self):
+        client = Client()
+        response = client.post('/', {'filterFilesForm': 'something', 'filter_name': 'something'})
+        self.assertEqual(response.status_code, 200)
