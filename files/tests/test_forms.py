a4587ab47065476414c9daaefcd04e906ca4c6c7
shlomi <shlomitofahi@gmail.com>
2019-05-03 23:19:06 +0300

    T19-227 uni-test test_sort_form

--- files/tests/test_forms.py
+++ files/tests/test_forms.py
@@ -4,6 +4,8 @@ from files.forms import filterFilesForm
 
 
 class MyTests(TestCase):
+    def setUp(self):
+        self.form = filterFilesForm({'1': '2'})
 
     def test_filterFilesForm_forms(self):
         form_data = {'something': 'something'}
@@ -14,3 +16,9 @@ class MyTests(TestCase):
         client = Client()
         response = client.post('/', {'filterFilesForm': 'something', 'filter_name': 'something'})
         self.assertEqual(response.status_code, 200)
+
+    def test_sort_form(self):
+        response1 = self.client.get("/")
+        response2 = self.client.post("include url to post the data given", {'username': "testuser", 'category': "OOP", 'url': "files/media/files/test.pdf", 'owner': "test@test.com"})
+        self.assertTrue('"error": true' , response2.content)
+        self.assertEqual(response1.status_code, 200)
