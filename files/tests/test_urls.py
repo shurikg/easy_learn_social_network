d6a2aef0b0b4f2f5140fc1e59620a59df96b2efa
shlomi <shlomitofahi@gmail.com>
2019-05-03 18:48:31 +0300

    T19-225 uni-test url download_file

---
+++ files/tests/test_urls.py
@@ -0,0 +1,64 @@
+
+from coverage.annotate import os
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+from files.models import File
+from files.views import download_file, show_files
+from users.models import Course, Degree, UserCourses, UserDegrees, Privacy, Profile
+from users.views import User
+from django.utils import timezone
+
+
+class TestUrls(TestCase):
+
+    def setUp(self):
+        # create user
+        self.username = 'testuser'
+        self.email = 'test@test.com'
+        self.password = '12345'
+        self.user = User(username=self.username, email=self.email)
+        self.user.set_password(self.password)
+        self.user.save()
+        login = self.client.login(username=self.username, password=self.password)
+        self.assertEqual(login, True)
+
+        # create category
+        self.category = Course.objects.create(
+            course_id='1',
+            course_name='OOP'
+        )
+
+        # create degree
+        self.software_eng_degree = Degree.objects.create(
+            degree_id='1',
+            degree_name='Software Engineering'
+        )
+
+        self.social_worker_degree = Degree.objects.create(
+            degree_id='2',
+            degree_name='Social Worker'
+        )
+
+        # upload file
+        f = SimpleUploadedFile('test.pdf', b'test context')
+
+        # create file model object
+        self.file1 = File(
+            category=self.category,
+            create_at=timezone.now,
+            file_url=f,
+            owner=self.user,
+        )
+        self.file1.save()
+        self.file1.related_degrees.add(self.software_eng_degree)
+        self.file1.related_degrees.add(self.social_worker_degree)
+
+    def test_download_file_url_is_resolved(self):
+        file_id = self.file1.id
+        url = reverse('files:download_file', args=(file_id,))
+        self.assertEqual(resolve(url).func, download_file)
+
+    def tearDown(self):
+        os.remove(self.file1.file_url.path)
