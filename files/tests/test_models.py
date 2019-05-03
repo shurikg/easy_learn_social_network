7972f7f8c3eb2ec0164c842878e3960c69467687
Yoni <jonbir3@gmail.com>
2019-05-03 20:20:30 +0300

    T19-290 create unit test of raise exception while save illegal file

--- files/tests/test_models.py
+++ files/tests/test_models.py
@@ -1,5 +1,5 @@
 import os
-
+from django.core.exceptions import ValidationError
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.contrib.auth.models import User
@@ -39,49 +39,63 @@ class TestModels(TestCase):
             degree_name='Social Worker'
         )
 
-        # upload file
-        f = SimpleUploadedFile('test.pdf', b'test context')
+        # upload legal file
+        legal = SimpleUploadedFile('test.pdf', b'test context')
 
         # create file model object
-        self.file1 = File(
+        self.legal_file = File(
             category=self.category,
             create_at=timezone.now(),
-            file_url=f,
+            file_url=legal,
             owner=self.user,
         )
-        self.file1.save()
-        self.file1.related_degrees.add(self.software_eng_degree)
-        self.file1.related_degrees.add(self.social_worker_degree)
+        self.legal_file.save()
+        self.legal_file.related_degrees.add(self.software_eng_degree)
+        self.legal_file.related_degrees.add(self.social_worker_degree)
 
     def test_file_extension(self):
-        self.assertEqual(self.file1.file_type, 'pdf')
+        self.assertEqual(self.legal_file.file_type, 'pdf')
+
+    def test_upload_file_not_allowed(self):
+        # upload illegal file
+        illegal = SimpleUploadedFile('test.py', b'print("Wrong!")')
+
+        # create file model object
+        self.illegal_file = File(
+            category=self.category,
+            create_at=timezone.now(),
+            file_url=illegal,
+            owner=self.user,
+        )
+        with self.assertRaises(ValidationError):
+            self.illegal_file.save()
 
     def test_name_start_with_id(self):
-        file_id = str(self.file1.id)
-        self.assertTrue(self.file1.file_name.startswith(file_id))
+        file_id = str(self.legal_file.id)
+        self.assertTrue(self.legal_file.file_name.startswith(file_id))
 
     def test_file_category(self):
-        self.assertEqual(self.file1.category, self.category)
+        self.assertEqual(self.legal_file.category, self.category)
 
     def test_related_degrees(self):
         degrees = (self.social_worker_degree.degree_name, self.software_eng_degree.degree_name)
-        for degree in self.file1.related_degrees.all():
+        for degree in self.legal_file.related_degrees.all():
             self.assertTrue(degree.degree_name in degrees)
         for degree_name in degrees:
-            self.assertTrue(degree_name in (d.degree_name for d in self.file1.related_degrees.all()))
+            self.assertTrue(degree_name in (d.degree_name for d in self.legal_file.related_degrees.all()))
 
     def test_file_name_contains_owner(self):
         owner_username = self.user.username
-        self.assertTrue(owner_username in self.file1.file_name)
+        self.assertTrue(owner_username in self.legal_file.file_name)
 
     def test_owner(self):
-        self.assertEqual(self.file1.owner, self.user)
+        self.assertEqual(self.legal_file.owner, self.user)
 
     def test_upload_at(self):
-        self.assertEqual(self.file1.upload_at.date(), timezone.now().date())
+        self.assertEqual(self.legal_file.upload_at.date(), timezone.now().date())
 
     def test_file_size(self):
-        self.assertEqual(self.file1.file_size, '12 B')
+        self.assertEqual(self.legal_file.file_size, '12 B')
 
     def tearDown(self):
-        os.remove(self.file1.file_url.path)
+        os.remove(self.legal_file.file_url.path)
