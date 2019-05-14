0c973858ac8ec0ac12cc5196d3b929a3f7607d64
elishalmon <elishalmon111@gmail.com>
2019-05-14 03:36:17 +0300

    t19-201 test admin message

---
+++ chat/tests/test_views.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from chat.models import Message, MessageManager
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        self.outbox_url = reverse('chat:messages_outbox')
+        self.compose_url = reverse('chat:messages_compose')
+
+        # create sender
+        recipient_username = 'sender'
+        recipient_email = 'sender@test.com'
+        recipient_password = '12345'
+        self.sender = User(username=recipient_username, email=recipient_email)
+        self.sender.set_password(recipient_password)
+        self.sender.save()
+        login = self.client.login(username=recipient_username, password=recipient_password)
+        self.assertEqual(login, True)
+
+        # create recipient
+        recipient_username = 'recipient'
+        recipient_email = 'recipient@test.com'
+        recipient_password = '12345'
+        self.recipient = User(username=recipient_username, email=recipient_email)
+        self.recipient.set_password(recipient_password)
+        self.recipient.save()
+        login = self.client.login(username=recipient_username, password=recipient_password)
+        self.assertEqual(login, True)
+
+        # create message
+        self.message = Message.objects.create(
+            subject='test subject',
+            body='test body',
+            sender=self.sender,
+            recipient=self.recipient,
+        )
+
+    def test_test_view(self):
+        assert 1 == 1
+
+    def test_inbox_view(self):
+        response = self.client.post(self.outbox_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'chat/outbox.html')
