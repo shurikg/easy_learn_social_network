aadba0a6ef85b266546e2f57d7b364b4afecfb5a
Yoni <jonbir3@gmail.com>
2019-05-20 12:51:04 +0300

    T19-320 add program to sending emails

--- send_mail.py
+++ send_mail.py
@@ -2,25 +2,22 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
 import os
-from sys import argv
+
+mail_password = os.environ.get('PASS_ONLY')
 
 
-def send_mail(from_user, to_user, subject, body):
+def send_mail(from_user, to_user, subject, body, cc=None):
     msg = MIMEMultipart()
     msg['From'] = from_user
     msg['To'] = to_user
     msg['Subject'] = subject
+    rcpt = to_user
+    if cc:
+        msg['Cc'] = cc
+        rcpt = [to_user] + cc.split(',')
     msg.attach(MIMEText(body, 'html'))
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login('des.sce.il19@gmail.com', mail_password)
-    server.sendmail(msg['From'], msg['to'], msg.as_string())
+    server.sendmail(msg['From'], rcpt, msg.as_string())
     server.quit()
-
-
-if __name__ == '__main__':
-    mail_password = os.environ.get('PASS_ONLY')
-    try:
-        send_mail('des.sce.il19@gmail.com', argv[1], argv[2], argv[3])
-    except Exception as e:
-        print(e)
