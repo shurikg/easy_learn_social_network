365dd061b284195a41c2c03fb6a3235e2cbf6c8b
Yoni <jonbir3@gmail.com>
2019-05-20 12:53:41 +0300

    T19-321 add recipient details and the mail details

--- send_mail.py
+++ send_mail.py
@@ -2,6 +2,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
 import os
+from sys import argv
 
 mail_password = os.environ.get('PASS_ONLY')
 
@@ -21,3 +22,14 @@ def send_mail(from_user, to_user, subject, body, cc=None):
     server.login('des.sce.il19@gmail.com', mail_password)
     server.sendmail(msg['From'], rcpt, msg.as_string())
     server.quit()
+
+
+if __name__ == '__main__':
+    try:
+        arg_to = 'jonbir2@gmail.com'
+        arg_cc = 'shlomitofahi@gmail.com,mayhagbi@gmail.com,elishalmon111@gmail.com'
+        arg_subject = argv[1]
+        arg_body = argv[2]
+        send_mail('des.sce.il19@gmail.com', arg_to, arg_subject, arg_body, arg_cc)
+    except Exception as e:
+        print(e)
